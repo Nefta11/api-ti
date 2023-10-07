e2d6c31b411f13566f350f3c6c3dfc7391d5aa5e
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-07 10:16:50 -0600

    actualizacion

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express=require('express')
 
 const app=express();
 
+app.get('/insert',(req,res)=>{
+res.json({status:"peticion recibida desde el equipo de Neftali"});
+});
+
 app.listen(3000,()=> {
     console.log('server listening on port 3000');
 })
