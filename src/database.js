6c0189022dbeb6a3b80ded9765aedde26a7d2e5b
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-07 11:11:57 -0600

    actualizacion...1

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Nefta11:<7641146446Nefta>@atlascluster.3mhp7ar.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp')
+mongoose.connect('mongodb+srv://Nefta11:7641146446Nefta@atlascluster.3mhp7ar.mongodb.net/ticontrol?retryWrites=true&w=majority&appName=AtlasApp')
 .then(db=> console.log('Mongodb atlas connected'))
 .catch(err=>console.error(err));
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const app=require('./app');
 require('./database');
 
 
-app.listen(app,get('PORT'),()=>{
+app.listen(app.get('PORT'),()=>{
     console.log('Server on port: '+ app.get('PORT'));
 });
